18ac702750c4c02c570f4479150cea8fb0499f25
Johan Henkens <johan@henkens.com>
2023-07-25 11:17:07 -0700

    Fix template generation

--- build_homeassistant_sensors.py
+++ build_homeassistant_sensors.py
@@ -10,7 +10,7 @@ def build_homeassistant_rest_sensor_template(converted_data):
     return f"""
 rest:
   - scan_interval: 30
-    resource: "{server_addr}"
+    resource: "{server_addr}/ha"
     sensor:
 {sensors}
 """.strip()
@@ -18,11 +18,12 @@ rest:
 def sensor_data(entry):
     lines = {
         "name": f"ATT RG {entry['name']}",
-        "value_template": f"{{ value_json.{entry['id']} }}",
+        "value_template": "{{ value_json." + entry['id'] +" }}",
         "unique_id": f"att_rg_rest_{ entry['id'] }",
     }
     if isinstance(entry["value"], float):
         lines["state_class"] = "measurement"
+        lines["value_template"] = "{{ value_json." + entry['id'] + " | float }}"
     if entry.get("unit"):
         lines["unit_of_measurement"] = entry["unit"]
     lines = [f'{k}: "{v}"' for k,v in lines.items()]
